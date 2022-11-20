6cd667f4486931dede4b0f0291088374876e1ef9
Sukrit Saha <106579296+Sukrittt@users.noreply.github.com>
2022-11-20 22:27:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
-var flag = false;
-
+// When clicked on the toggle icon
 document.getElementById("toggler-icon").addEventListener("click", function (e) {
   if (
     document.getElementById("collapse-items").style.display == "" ||
     document.getElementById("collapse-items").style.display == "none"
   ) {
     document.getElementById("collapse-items").style.display = "flex";
+    document.getElementById("nav-item").style.transition = "1s";
   } else {
+    document.getElementById("collapse-items").style.display = "none";
+    document.getElementById("collapse-items").style.transition = "1s";
+  }
+});
+
+// When Clicked outside of nav links
+document.getElementById("main").addEventListener("click", function (e) {
+  console.log("clicked");
+  if (document.getElementById("collapse-items").style.display == "flex") {
     document.getElementById("collapse-items").style.display = "none";
   }
 });
